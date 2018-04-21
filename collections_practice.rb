f17739b03c25a4a8b9e2c5cb732f2daf9c3f036b
Chris Alberti <christopherjalberti@gmail.com>
2018-04-21 23:53:44 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|x| x.start_with?("r")}
+end
+
+def contain_a(array)
+  array.select {|x| x.include?("a")}
+end
+
+def first_wa(array)
+  array.find {|x| x.to_s.start_with?("wa")}
+end
+
+def remove_non_strings(array)
+  array.select {|x| x == x.to_s}
+end
+
+def count_elements(array)
+  result = []
+  counted = []
+
+  array.each do |hash|
+    count = array.count(hash)
+    hash[:count] = count
+    result << hash if !counted.include?(hash[:name])
+    counted << hash[:name]
+  end
+  result
+end
+
+def merge_data(keys, data)
+  
+end
+
+def organize_schools(schools)
+
+end
